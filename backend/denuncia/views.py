7dec0be3c107131085b178f3e126cb8d9da19a97
AnthonyGuaman2002 <aao.guaman@yavirac.edu.ec>
2023-09-12 14:09:09 -0500

    Login

--- backend/denuncia/views.py
+++ backend/denuncia/views.py
@@ -1,7 +1,74 @@
 from rest_framework import viewsets
 from .serializer import DenunciaSerializer
 from .models import Denuncia
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+from django.db import IntegrityError
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def signup(request):
+
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+            'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # registrar usuario
+                user = User.objects.create_user(username=request.POST['username'],
+                                                password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('denuncias')
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': 'Usuario ya exite'
+                })
+
+        return render(request, 'signup.html', {
+            'form': UserCreationForm,
+            'error': 'Contraseñas no coinciden'
+        })
+
+@login_required
+def denuncias(request):
+    return render(request, 'denuncias.html')
+
+@login_required
+def signout(request):
+    logout(request)
+    return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST
+                            ['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'El usuario o la contraseña es incorrecto'
+            })
+        else:
+            login(request, user)
+            return redirect('denuncias')
+
+
 class DenunciaView(viewsets.ModelViewSet):
     serializer_class = DenunciaSerializer
     queryset = Denuncia.objects.all()
